1b3514378ecf29b780d87f6c4d350dc0b31c59f6
havardla <46992761+havardla@users.noreply.github.com>
2019-01-24 15:10:54 +0100

    update

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -46,7 +46,7 @@ namespace StarChart.Controllers
             var celestialObjects = _context.CelestialObjects.ToList();
             foreach (var celestialObject in celestialObjects)
             {
-                celestialObject.Satellites = _context.CelestialObjects.Where(e=>e.OrbitedObjectId==celestialObject.Id).ToList()
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == celestialObject.Id).ToList();
             }
             return Ok(celestialObjects);
         }
